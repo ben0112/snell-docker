862d8706d560b4cbd7df0fe86366dbd2a8adb593
ben0112 <ben0112@users.noreply.github.com>
2020-09-18 19:24:48 +0800

    Create get_snell.sh

---
+++ get_snell.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+url=$(wget -qO- https://api.github.com/repos/surge-networks/snell/releases/latest | grep -e "browser_download_url" | grep linux-amd64 | grep -o 'https*://[^"]*')
+wget --no-check-certificate -O snell.zip $url
+unzip snell.zip
+rm snell.zip
+chmod +x snell-server
+mv snell-server /usr/bin/
